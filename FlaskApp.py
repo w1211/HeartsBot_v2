6e0915191c7cb4ead38cb97c616152dd0fede63b
Tom Wells <tom.wells96@gmail.com>
2017-11-18 17:21:04 +1100

    First commit of new heartsBot!

---
+++ FlaskApp.py
@@ -0,0 +1,12 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8089)
